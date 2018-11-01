ddb1728644a0cbbd0f6c1a33347a076fa93d2cfd
weyusi <weyusi@users.noreply.github.com>
2018-11-01 12:36:14 +1030

    refactor: use ES6 syntax
    var to let/const
    arrow functions

--- test/index.js
+++ test/index.js
@@ -1,50 +1,50 @@
 'use strict';
 
-var should = require('chai').should(); // eslint-disable-line
-var fs = require('hexo-fs');
-var pathFn = require('path');
+const should = require('chai').should(); // eslint-disable-line
+const fs = require('hexo-fs');
+const pathFn = require('path');
 
-describe('EJS renderer', function() {
-  var r = require('../lib/renderer');
+describe('EJS renderer', () => {
+  const r = require('../lib/renderer');
 
-  it('default', function() {
-    var body = 'Hello <%= name %>';
-    var result = r({text: body}, {name: 'world'});
+  it('default', () => {
+    const body = 'Hello <%= name %>';
+    const result = r({text: body}, {name: 'world'});
 
     result.should.eql('Hello world');
   });
 
-  it('comments', function() {
-    var body = [
+  it('comments', () => {
+    const body = [
       'Comment <%# hidden %>'
     ].join('\n');
 
-    var result = r({text: body});
+    const result = r({text: body});
     result.should.eql('Comment ');
   });
 
-  it('include', function() {
-    var body = '<% include test %>';
-    var path = pathFn.join(__dirname, 'include_test', 'index.ejs');
-    var includePath = pathFn.join(path, '../test.ejs');
-    var includeBody = 'include body';
+  it('include', () => {
+    const body = '<% include test %>';
+    const path = pathFn.join(__dirname, 'include_test', 'index.ejs');
+    const includePath = pathFn.join(path, '../test.ejs');
+    const includeBody = 'include body';
 
-    return fs.writeFile(includePath, includeBody).then(function() {
-      var result = r({
+    return fs.writeFile(includePath, includeBody).then(() => {
+      const result = r({
         text: body,
         path: path
       });
 
       result.should.eql(includeBody);
-    }).finally(function() {
+    }).finally(() => {
       return fs.unlink(includePath);
     });
   });
 
-  it('compile', function() {
-    var body = 'Hello <%= name %>';
-    var render = r.compile({text: body});
-    var result = render({
+  it('compile', () => {
+    const body = 'Hello <%= name %>';
+    const render = r.compile({text: body});
+    const result = render({
       name: 'world'
     });
 
